22d6d64c1feaf8de63ebd23eb91ad2aaf1f30d58
SadabQ <shadab.qureshi88@gmail.com>
2017-05-15 15:31:52 -0700

    Add files via upload

---
+++ Fitbit/src/main/java/com/fitbit/model/DailySleep.java
@@ -0,0 +1,27 @@
+package com.fitbit.model;
+
+import java.util.Date;
+
+public class DailySleep 
+{
+	private int totalMinutesAsleep ;
+	
+	private String datesleep;
+
+	public String getDatesleep() {
+		return datesleep;
+	}
+
+	public void setDatesleep(String datesleep) {
+		this.datesleep = datesleep;
+	}
+
+	public int getTotalMinutesAsleep() {
+		return totalMinutesAsleep;
+	}
+
+	public void setTotalMinutesAsleep(int totalMinutesAsleep) {
+		this.totalMinutesAsleep = totalMinutesAsleep;
+	}
+	
+}
